5747a81e454ec77a576f1ce06b285219f013294e
Tim Vinkemeier <timvinkemeier@live.de>
2015-06-02 22:22:26 +0200

    Fixed Navigation Attribute. Added INavigationAwareTargetContainer.

---
+++ Swift.Extensibility/UI/INavigationAwareTargetContainer.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.Composition;
+
+namespace Swift.Extensibility.UI
+{
+    /// <summary>
+    /// Interface for Navigation-aware NavigationTargetContainers.
+    /// </summary>
+    [InheritedExport]
+    public interface INavigationAwareTargetContainer
+    {
+        /// <summary>
+        /// Gets called before an incoming navigation happens.
+        /// </summary>
+        /// <param name="viewModel">The viewmodel object.</param>
+        /// <param name="target">The target that the viewmodel should be navigated to.</param>
+        /// <returns>A <see cref="NavigationHandlerResult"/> value indicating how navigation should proceed.</returns>
+        NavigationHandlerResult OnIncomingNavigation(object viewModel, string target);
+
+        /// <summary>
+        /// Gets called before an outgoing navigation happens.
+        /// </summary>
+        /// <param name="target">The target that the viewmodel should be navigated from.</param>
+        /// <returns>A <see cref="NavigationHandlerResult"/> value indicating how navigation should proceed.</returns>
+        NavigationHandlerResult OnOutgoingNavigation(string target);
+    }
+}
---
+++ Swift.Extensibility/UI/NavigationHandlerResult.cs
@@ -0,0 +1,21 @@
+﻿namespace Swift.Extensibility.UI
+{
+    /// <summary>
+    /// Contains results for navigation handlers.
+    /// </summary>
+    public enum NavigationHandlerResult
+    {
+        /// <summary>
+        /// Indicates that the navigation request should not be processed further and has not been handled by the handler.
+        /// </summary>
+        CancelNavigation,
+        /// <summary>
+        /// Indicates that the navigation framework can continue processing the navigation request.
+        /// </summary>
+        ContinueNavigation,
+        /// <summary>
+        /// Indicates that the handler has already handled the navigation request.
+        /// </summary>
+        NavigationHandled
+    }
+}

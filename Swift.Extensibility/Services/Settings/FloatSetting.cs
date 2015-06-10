212dae8bf54b04c81cd1818895921951bfc0771d
Tim Vinkemeier <timvinkemeier@live.de>
2015-06-10 22:55:02 +0200

    Added float and function settings.

---
+++ Swift.Extensibility/Services/Settings/FloatSetting.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Swift.Toolkit;
+
+namespace Swift.Extensibility.Services.Settings
+{
+    /// <summary>
+    /// Represents a setting that has a float value. Can be clamped between min and max values.
+    /// </summary>
+    public sealed class FloatSetting : BindableBase, ISetting<float>
+    {
+        private float _value;
+        /// <summary>
+        /// Gets or sets the value.
+        /// </summary>
+        public float Value
+        {
+            get
+            {
+                return _value;
+            }
+            set
+            {
+                if (value != _value && value >= MinValue && value <= MaxValue)
+                {
+                    Set(ref _value, value);
+                    ChangeCallback?.Invoke(value);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the default value.
+        /// </summary>
+        public float DefaultValue { get; }
+
+        /// <summary>
+        /// Gets the minimum value.
+        /// </summary>
+        /// <value>
+        /// The minimum value.
+        /// </value>
+        public float MinValue { get; }
+
+        /// <summary>
+        /// Gets the maximum value.
+        /// </summary>
+        /// <value>
+        /// The maximum value.
+        /// </value>
+        public float MaxValue { get; }
+
+        /// <summary>
+        /// Gets the change callback.
+        /// </summary>
+        /// <value>
+        /// The change callback.
+        /// </value>
+        public Action<float> ChangeCallback { get; }
+
+        /// <summary>
+        /// Gets or sets the display name.
+        /// </summary>
+        public string DisplayName { get; }
+
+        /// <summary>
+        /// Gets or sets the tool tip. Can be null to hide tooltips.
+        /// </summary>
+        public string ToolTip { get; }
+
+        /// <summary>
+        /// Gets or sets the description. Can be null to hide the description.
+        /// </summary>
+        public string Description { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FloatSetting"/> class.
+        /// </summary>
+        /// <param name="displayName">The display name.</param>
+        /// <param name="defaultValue">The default value.</param>
+        /// <param name="changeCallback">The change callback.</param>
+        /// <param name="value">The value.</param>
+        /// <param name="tooltip">The tooltip.</param>
+        /// <param name="description">The description.</param>
+        /// <param name="minValue">The minimum value.</param>
+        /// <param name="maxValue">The maximum value.</param>
+        public FloatSetting(string displayName, float defaultValue, Action<float> changeCallback, float value = 0, string tooltip = "", string description = "", float minValue = float.MinValue, float maxValue = float.MaxValue)
+        {
+            DisplayName = displayName;
+            Value = value;
+            ChangeCallback = changeCallback;
+            DefaultValue = defaultValue;
+            ToolTip = tooltip;
+            Description = description;
+            MinValue = minValue;
+            MaxValue = maxValue;
+        }
+    }
+}
---
+++ Swift.Extensibility/Services/Settings/FunctionSetting.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Swift.Extensibility.Input.Functions;
+
+namespace Swift.Extensibility.Services.Settings
+{
+    /// <summary>
+    /// Represents a setting with a FunctionInfo value.
+    /// </summary>
+    public sealed class FunctionSetting : Setting<FunctionInfo>
+    {
+        /// <summary>
+        /// The filter that selects which functions are selectable.
+        /// </summary>
+        public Predicate<FunctionInfo> Filter { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FunctionSetting"/> class.
+        /// </summary>
+        /// <param name="displayName">The display name.</param>
+        /// <param name="defaultValue">if set to <c>true</c> [default value].</param>
+        /// <param name="changeCallback">The change callback.</param>
+        /// <param name="value">if set to <c>true</c> [value].</param>
+        /// <param name="tooltip">The tooltip.</param>
+        /// <param name="description">The description.</param>
+        /// <param name="filter">A filter to filter out not-selectable functions. By default, all functions will be available.</param>
+        public FunctionSetting(string displayName, FunctionInfo defaultValue, Action<FunctionInfo> changeCallback, FunctionInfo value = null, string tooltip = "", string description = "", Predicate<FunctionInfo> filter = null)
+        {
+            DisplayName = displayName;
+            Value = value;
+            ChangeCallback = changeCallback;
+            DefaultValue = defaultValue;
+            ToolTip = tooltip;
+            Description = description;
+            Filter = filter ?? (_ => true);
+        }
+    }
+}

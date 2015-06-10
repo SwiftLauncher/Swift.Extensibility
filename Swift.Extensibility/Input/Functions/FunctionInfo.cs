13f8af27b1d1af8c95bc813dc9760d9d7f56ac47
Tim Vinkemeier <timvinkemeier@live.de>
2015-06-10 22:50:28 +0200

    Added function and parameter info classes.

---
+++ Swift.Extensibility/Input/Functions/FunctionInfo.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace Swift.Extensibility.Input.Functions
+{
+    /// <summary>
+    /// Represents a Swift function.
+    /// </summary>
+    public sealed class FunctionInfo
+    {
+        /// <summary>
+        /// The full name of the function, including subfunction qualifiers.
+        /// </summary>
+        public string FullName { get; }
+
+        /// <summary>
+        /// The function name qualifiers.
+        /// </summary>
+        public IEnumerable<string> NameParts { get; }
+
+        /// <summary>
+        /// The call mode.
+        /// </summary>
+        public FunctionCallMode CallMode { get; }
+
+        /// <summary>
+        /// The list of parameters.
+        /// </summary>
+        public IEnumerable<ParameterInfo> Parameters { get; }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="FunctionInfo"/> class.
+        /// </summary>
+        /// <param name="fullName">The full name of the function.</param>
+        /// <param name="callMode">The callmode of the function.</param>
+        /// <param name="parameters">The list of parameters of the function.</param>
+        public FunctionInfo(string fullName, FunctionCallMode callMode, IEnumerable<ParameterInfo> parameters)
+        {
+            FullName = fullName;
+            NameParts = FullName.TrimStart('.').Split('.');
+            CallMode = callMode;
+            Parameters = parameters;
+        }
+    }
+}
---
+++ Swift.Extensibility/Input/Functions/ParameterInfo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Swift.Extensibility.Input.Functions
+{
+    /// <summary>
+    /// Represents a function parameter.
+    /// </summary>
+    public sealed class ParameterInfo
+    {
+        /// <summary>
+        /// The name of the parameter.
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// The type of the parameter.
+        /// </summary>
+        public Type Type { get; }
+
+        /// <summary>
+        /// The description of the parameter.
+        /// </summary>
+        public string Description { get; }
+
+        /// <summary>
+        /// An example of the usage of this parameter.
+        /// </summary>
+        public string Example { get; }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="ParameterInfo"/> class.
+        /// </summary>
+        /// <param name="name">The name of the parameter.</param>
+        /// <param name="type">The type of the parameter.</param>
+        /// <param name="description">The description of the parameter.</param>
+        /// <param name="example">An example usage of the parameter.</param>
+        public ParameterInfo(string name, Type type, string description, string example)
+        {
+            Name = name;
+            Type = type;
+            Description = description;
+            Example = example;
+        }
+    }
+}

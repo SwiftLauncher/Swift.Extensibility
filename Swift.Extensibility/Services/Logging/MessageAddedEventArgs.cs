47d4275e55feb0007f7020ad033b7c9c1029911c
Tim Vinkemeier <timvinkemeier@live.de>
2015-06-10 22:53:08 +0200

    Moved logging to MEF approach.

---
+++ Swift.Extensibility/Services/Logging/MessageAddedEventArgs.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Swift.Extensibility.Services.Logging
+{
+    /// <summary>
+    /// EventArgs for MessageAdded events.
+    /// </summary>
+    public class MessageAddedEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Gets the message.
+        /// </summary>
+        /// <value>
+        /// The message.
+        /// </value>
+        public LogMessage Message { get; }
+
+        /// <summary>
+        /// Gets the channel identifier.
+        /// </summary>
+        /// <value>
+        /// The channel identifier.
+        /// </value>
+        public string ChannelId { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MessageAddedEventArgs"/> class.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <param name="channelId">The channel identifier.</param>
+        public MessageAddedEventArgs(LogMessage message, string channelId)
+        {
+            Message = message;
+            ChannelId = channelId;
+        }
+    }
+}

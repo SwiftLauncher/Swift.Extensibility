86b7e5e641c1eb7a3887ce2c18c946fc6643c0d1
Tim Vinkemeier <timvinkemeier@live.de>
2015-07-01 22:17:33 +0200

    Added AppVeyor CI configuration.
    Swift.Extensibility will now be automatically build and deployed to
    NuGet!

---
+++ NuGet/Create-Package.ps1
@@ -0,0 +1,16 @@
+$project = "Swift.Extensibility"
+$buildNumberPrefix = "0-alpha"
+
+$root = (split-path -parent $MyInvocation.MyCommand.Definition) + '\..'
+$version = [System.Reflection.Assembly]::LoadFile("$root\$project\bin\Release\$project.dll").GetName().Version
+$versionStr = "1.0.$buildNumberPrefix{0}" -f ($version.Build + 100)
+
+Write-Host "Setting .nuspec version tag to $versionStr"
+
+$content = (Get-Content $root\NuGet\$project.nuspec) 
+$content = $content -replace '\$version\$',$versionStr
+
+$content | Out-File $root\nuget\$project.compiled.nuspec
+
+& $root\NuGet\NuGet.exe pack $root\nuget\$project.compiled.nuspec
+Remove-Item $root\nuget\$project.compiled.nuspec
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Swift.Extensibility
+
+[![Build status](https://ci.appveyor.com/api/projects/status/oqnwyyu4ynspin54?svg=true)](https://ci.appveyor.com/project/tnv/swift-extensibility)
+
 Repository for the Swift.Extensibility NuGet package for developing Swift plugins.
